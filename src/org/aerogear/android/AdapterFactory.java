586c8ea890dfd266bd54d7104ebd1007f9582b11
Summers Pittman <supittma@redhat.com>
2012-11-05 18:13:55 -0500

    GsonBuilder is now a PipeConfig property

--- src/org/aerogear/android/AdapterFactory.java
+++ src/org/aerogear/android/AdapterFactory.java
@@ -18,12 +18,11 @@
 package org.aerogear.android;
 
 import android.util.Log;
-import com.google.gson.GsonBuilder;
 import org.aerogear.android.datamanager.IdGenerator;
-import org.aerogear.android.impl.datamanager.MemoryStorage;
 import org.aerogear.android.datamanager.Store;
-import org.aerogear.android.impl.datamanager.StoreType;
 import org.aerogear.android.impl.core.HttpRestProvider;
+import org.aerogear.android.impl.datamanager.MemoryStorage;
+import org.aerogear.android.impl.datamanager.StoreType;
 import org.aerogear.android.impl.pipeline.PipeConfig;
 import org.aerogear.android.impl.pipeline.RestAdapter;
 import org.aerogear.android.impl.pipeline.Types;
@@ -36,17 +35,16 @@ import static org.aerogear.android.impl.datamanager.StoreType.MEMORY;
 
 final class AdapterFactory {
 
-    private AdapterFactory() {}
+    private AdapterFactory() {
+    }
 
     public static Pipe createPipe(Class klass, PipeConfig config) {
         if (config.getType().equals(Types.REST)) {
-            return new RestAdapter(klass, new HttpRestProvider(appendEndpoint(config.getBaseURL(), config.getEndpoint())));
-        }
-        throw new IllegalArgumentException("Type is not supported yet");
-    }
-    public static Pipe createPipe(Type type, Class klass, URL url, GsonBuilder builder) {
-        if (type.equals(Type.REST)) {
-                return new RestAdapter(klass, new HttpRestProvider(url), builder);
+            if (config.getGsonBuilder() == null) {
+                return new RestAdapter(klass, new HttpRestProvider(appendEndpoint(config.getBaseURL(), config.getEndpoint())));
+            } else {
+                return new RestAdapter(klass, new HttpRestProvider(appendEndpoint(config.getBaseURL(), config.getEndpoint())), config.getGsonBuilder());
+            }
         }
         throw new IllegalArgumentException("Type is not supported yet");
     }
@@ -61,7 +59,7 @@ final class AdapterFactory {
     private static URL appendEndpoint(URL baseURL, String endpoint) {
 
         try {
-            if( !baseURL.toString().endsWith("/")) {
+            if (!baseURL.toString().endsWith("/")) {
                 endpoint = "/" + endpoint;
             }
             return new URL(baseURL + endpoint + "/");
