9b30836b50c47bee47da4eb6011c36d31ba4cf20
Summers Pittman <supittma@redhat.com>
2013-01-21 08:49:49 -0500

    WebLinkParser

---
+++ src/org/jboss/aerogear/android/impl/util/ParseException.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.jboss.aerogear.android.impl.util;
+
+/**
+ *
+ * @author summers
+ */
+public class ParseException extends Exception {
+
+}
