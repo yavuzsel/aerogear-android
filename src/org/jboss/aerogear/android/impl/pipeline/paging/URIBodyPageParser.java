eb0867425a4b88496a637436ef8be903df320bff
Summers Pittman <supittma@redhat.com>
2013-01-27 22:46:48 -0500

    Adding license information to files

--- src/org/jboss/aerogear/android/impl/pipeline/paging/URIBodyPageParser.java
+++ src/org/jboss/aerogear/android/impl/pipeline/paging/URIBodyPageParser.java
@@ -1,6 +1,18 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * JBoss, Home of Professional Open Source
+ * Copyright Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.jboss.aerogear.android.impl.pipeline.paging;
 
@@ -15,10 +27,6 @@ import org.jboss.aerogear.android.http.HeaderAndBody;
 import org.jboss.aerogear.android.pipeline.paging.PageConfig;
 import org.jboss.aerogear.android.pipeline.paging.PageResultExtractor;
 
-/**
- *
- * @author summers
- */
 public class URIBodyPageParser implements PageResultExtractor<PageConfig> {
 private final URI baseUri;
     
