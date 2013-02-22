838b5675b048e8ac459f316ec531cae59eb2e81f
Summers Pittman <supittma@redhat.com>
2013-02-22 13:33:04 -0500

    Add Loaders and a Pipe Adapter for android.support.v4 loaders. This follows the style of the previous "Modern" loaders

---
+++ src/org/jboss/aerogear/android/impl/pipeline/loader/support/AbstractSupportPipeLoader.java
@@ -0,0 +1,47 @@
+/**
+ * JBoss, Home of Professional Open Source
+ * Copyright Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.aerogear.android.impl.pipeline.loader.support;
+
+import android.content.Context;
+import android.support.v4.content.AsyncTaskLoader;
+import org.jboss.aerogear.android.Callback;
+
+public abstract class AbstractSupportPipeLoader<T> extends AsyncTaskLoader<T> {
+
+    public final Callback<T> callback;
+    protected Exception exception;
+
+    public AbstractSupportPipeLoader(Context context, Callback<T> callback) {
+        super(context);
+        this.callback = callback;
+    }
+
+    public boolean hasException() {
+        return exception != null;
+    }
+
+    public Exception getException() {
+        return exception;
+    }
+
+    @Override
+    protected void onReset() {
+        super.onReset();
+        exception = null;
+    }
+
+}
---
+++ src/org/jboss/aerogear/android/impl/pipeline/loader/support/SupportSaveLoader.java
@@ -0,0 +1,53 @@
+/**
+ * JBoss, Home of Professional Open Source
+ * Copyright Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.aerogear.android.impl.pipeline.loader.support;
+
+import android.content.Context;
+import org.jboss.aerogear.android.Callback;
+import org.jboss.aerogear.android.impl.pipeline.RestRunner;
+
+public class SupportSaveLoader<T> extends AbstractSupportPipeLoader<T> {
+
+    private final RestRunner<T> runner;
+    private final T data;
+    private T result;
+
+    public SupportSaveLoader(Context context, Callback<T> callback, RestRunner<T> runner, T data) {
+        super(context, callback);
+        this.runner = runner;
+        this.data = data;
+    }
+
+    @Override
+    public T loadInBackground() {
+        try {
+            return (result = runner.save(data));
+        } catch (Exception e) {
+            super.exception = e;
+        }
+        return null;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        if (result != null) {
+            deliverResult(result);
+        } else {
+            forceLoad();
+        }
+    }
+}
