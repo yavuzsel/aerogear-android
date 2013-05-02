f3bef918f3caf2566996b0a794c2e74daaa9594c
Summers Pittman <secondsun@gmail.com>
2013-05-02 11:55:19 -0400

    Javadocs

--- src/org/jboss/aerogear/android/authentication/impl/HttpBasicAuthenticationModule.java
+++ src/org/jboss/aerogear/android/authentication/impl/HttpBasicAuthenticationModule.java
@@ -1,6 +1,17 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * JBoss, Home of Professional Open Source Copyright Red Hat, Inc., and
+ * individual contributors by the
+ *
+ * @authors tag. See the copyright.txt in the distribution for a full listing of
+ * individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by
+ * applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+ * OF ANY KIND, either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
  */
 package org.jboss.aerogear.android.authentication.impl;
 
@@ -23,29 +34,41 @@ import org.jboss.aerogear.android.http.HeaderAndBody;
 
 import android.util.Base64;
 import android.util.Pair;
+import java.util.Map;
+import org.jboss.aerogear.android.pipeline.Pipe;
 
 /**
+ * This class provides Authentication using HTTP Basic
  *
- * @author summers
+ * As per the <a href="http://www.ietf.org/rfc/rfc2617.txt">HTTP RFC</a> this
+ * class will cache credentials and consumed by {@link Pipe} requests. This
+ * module assumes that credentials provided are valid and will never fail on {@link #login(java.lang.String, java.lang.String, org.jboss.aerogear.android.Callback)
+ * }
+ * or {@link AGSecurityAuthenticationModule#logout(org.jboss.aerogear.android.Callback)
+ * }.
+ *
+ * {@link #enroll(java.util.Map, org.jboss.aerogear.android.Callback) } is not
+ * supported and will always fail.
+ *
  */
 public class HttpBasicAuthenticationModule extends AbstractAuthenticationModule {
 
     private final static String BASIC_HEADER = "Authorization";
     private final static String AUTHORIZATION_METHOD = "Basic";
-    
     private final String loginEndpoint = "";
     private final String logoutEndpoint = "";
     private final String enrollEndpoint = "";
     private final URL baseURL;
-
     private boolean isLoggedIn = false;
     private PasswordAuthentication auth = new PasswordAuthentication("", new char[] {});
-    
+
+    /**
+     * @param baseURL The base URL shared by a Pipe.
+     */
     public HttpBasicAuthenticationModule(URL baseURL, AuthenticationConfig config) {
         this.baseURL = baseURL;
-        
     }
-    
+
     @Override
     public URL getBaseURL() {
         return baseURL;
@@ -71,20 +94,38 @@ public class HttpBasicAuthenticationModule extends AbstractAuthenticationModule 
         return isLoggedIn;
     }
 
+    /**
+     * This method stores username and password. Additionally, it sets
+     * {@link #isLoggedIn} to true and calls {@link Callback#onSuccess(java.lang.Object)
+     * }.
+     *
+     * @param username
+     * @param password
+     * @param callback
+     */
     @Override
     public void login(String username, String password, final Callback<HeaderAndBody> callback) {
         isLoggedIn = true;
         auth = new PasswordAuthentication(username, password.toCharArray());
         THREAD_POOL_EXECUTOR.execute(new Runnable() {
-
             @Override
             public void run() {
-                callback.onSuccess(new HeaderAndBody(new byte[]{}, new HashMap<String, Object>(1)));
+                callback.onSuccess(new HeaderAndBody(new byte[] {}, new HashMap<String, Object>(1)));
             }
         });
-        
+
     }
 
+    /**
+     *
+     * This method clears the username and password from the module, sets
+     * isLoggedIn to false, and removes all cookies associated with
+     * {@link #baseURL}
+     *
+     * This method always calls {@link Callback#onSuccess(java.lang.Object) }
+     *
+     * @param callback
+     */
     @Override
     public void logout(final Callback<Void> callback) {
         clearPassword(auth.getPassword());
@@ -92,30 +133,47 @@ public class HttpBasicAuthenticationModule extends AbstractAuthenticationModule 
         isLoggedIn = false;
 
         THREAD_POOL_EXECUTOR.execute(new Runnable() {
-
             @Override
             public void run() {
-            	try {
-					CookieStore store = ((CookieManager)CookieManager.getDefault()).getCookieStore();
-					List<HttpCookie> cookies = store.get(baseURL.toURI());
-					
-					for (HttpCookie cookie : cookies) {
-						store.remove(baseURL.toURI(), cookie);
-					}
-					
-					callback.onSuccess((Void) null);
-				} catch (URISyntaxException e) {
-					callback.onFailure(e);
-				}
-                
+                try {
+                    CookieStore store = ((CookieManager) CookieManager.getDefault()).getCookieStore();
+                    List<HttpCookie> cookies = store.get(baseURL.toURI());
+
+                    for (HttpCookie cookie : cookies) {
+                        store.remove(baseURL.toURI(), cookie);
+                    }
+
+                    callback.onSuccess((Void) null);
+                } catch (URISyntaxException e) {
+                    callback.onFailure(e);
+                }
+
             }
         });
-        
-        
+
     }
 
-    
-    
+    /**
+     *
+     * Enrolling is not supported using http basic.
+     *
+     * This method will call {@link Callback#onFailure(java.lang.Exception)} and pass
+     * it a UnsupportedOperationException.
+     *
+     * @param userData this value is ignored.
+     * @param callback
+     */
+    @Override
+    public void enroll(Map<String, String> userData, final Callback<HeaderAndBody> callback) {
+        THREAD_POOL_EXECUTOR.execute(new Runnable() {
+            @Override
+            public void run() {
+                callback.onFailure(new UnsupportedOperationException());
+            }
+        });
+
+    }
+
     @Override
     public AuthorizationFields getAuthorizationFields() {
         AuthorizationFields fields = new AuthorizationFields();
@@ -125,6 +183,12 @@ public class HttpBasicAuthenticationModule extends AbstractAuthenticationModule 
         return fields;
     }
 
+    /**
+     * HTTP Basic defines a base 64 encoded hash to be pass as a header to serve
+     * as authentication. This method calculates the value of that header.
+     *
+     * @return the http basic hash of the username and password
+     */
     private String getHashedAuth() {
         StringBuilder headerValueBuilder = new StringBuilder(AUTHORIZATION_METHOD).append(" ");
         String unhashedCredentials = new StringBuilder(auth.getUserName()).append(":").append(auth.getPassword()).toString();
@@ -132,10 +196,14 @@ public class HttpBasicAuthenticationModule extends AbstractAuthenticationModule 
         return headerValueBuilder.append(hashedCrentials).toString();
     }
 
+    /**
+     * This method replaces the characters in a character array with '0'.
+     *
+     * @param password a character array, usually a password
+     */
     private void clearPassword(char[] password) {
         for (int i = 0; i < password.length; i++) {
             password[i] = '0';
         }
     }
-    
 }
