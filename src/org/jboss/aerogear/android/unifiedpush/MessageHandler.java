55bf48edbed4c416feee1cd2f9893096e703bc17
Matthias Wessendorf <matzew@apache.org>
2013-07-20 15:43:22 +0200

    minor formating

--- src/org/jboss/aerogear/android/unifiedpush/MessageHandler.java
+++ src/org/jboss/aerogear/android/unifiedpush/MessageHandler.java
@@ -6,7 +6,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * 	http://www.apache.org/licenses/LICENSE-2.0
+ *  http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -26,7 +26,7 @@ public interface MessageHandler {
 
     /**
      * Invoked when the Google Cloud Messaging Server deleted some
-	 * pending messages because they were collapsible.
+     * pending messages because they were collapsible.
      *
      * @param context The Context in which the AeroGear message receiver is running.
      * @param message A map of extended data from the intent, delivered to the AeroGear message receiver.
