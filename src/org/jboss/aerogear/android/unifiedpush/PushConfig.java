102cc3e6f5ff9a71757600488fdd128fb77c7fad
Matthias Wessendorf <matzew@apache.org>
2013-07-31 22:59:48 +0200

    Formatting fixes

--- src/org/jboss/aerogear/android/unifiedpush/PushConfig.java
+++ src/org/jboss/aerogear/android/unifiedpush/PushConfig.java
@@ -6,7 +6,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * 	http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,7 +17,6 @@
 package org.jboss.aerogear.android.unifiedpush;
 
 import java.io.Serializable;
-
 
 import com.google.common.collect.ImmutableSet;
 
@@ -61,9 +60,9 @@ public class PushConfig implements Serializable {
      *
      * @return
      */
-	public String getVariantID() {
-		return variantID;
-	}
+    public String getVariantID() {
+        return variantID;
+    }
 
     /**
      * ID of the Variant from the AeroGear UnifiedPush Server.
@@ -93,89 +92,89 @@ public class PushConfig implements Serializable {
     }
 
     /**
-	 * Device type determines which cloud messaging system will be used by the
-	 * AeroGear Unified Push Server
-	 * 
-	 * Defaults to ANDROID
-	 */
-	public String getDeviceType() {
-		return deviceType;
-	}
+     * Device type determines which cloud messaging system will be used by the
+     * AeroGear Unified Push Server
+     * 
+     * Defaults to ANDROID
+     */
+    public String getDeviceType() {
+        return deviceType;
+    }
 
-	/**
-	 * Device type determines which cloud messaging system will be used by the
-	 * AeroGear Unified Push Server.
-	 * 
-	 * Defaults to ANDROID
-	 */
-	public void setDeviceType(String deviceType) {
-		this.deviceType = deviceType;
-	}
+    /**
+     * Device type determines which cloud messaging system will be used by the
+     * AeroGear Unified Push Server.
+     * 
+     * Defaults to ANDROID
+     */
+    public void setDeviceType(String deviceType) {
+        this.deviceType = deviceType;
+    }
 
-	/**
-	 * The name of the operating system. Defaults to Android
-	 */
-	public String getOperatingSystem() {
-		return operatingSystem;
-	}
+    /**
+     * The name of the operating system. Defaults to Android
+     */
+    public String getOperatingSystem() {
+        return operatingSystem;
+    }
 
-	/**
-	 * The name of the operating system. Defaults to Android
-	 */
-	public void setOperatingSystem(String operatingSystem) {
-		this.operatingSystem = operatingSystem;
-	}
+    /**
+     * The name of the operating system. Defaults to Android
+     */
+    public void setOperatingSystem(String operatingSystem) {
+        this.operatingSystem = operatingSystem;
+    }
 
-	/**
-	 * The version of the operating system running.
-	 * 
-	 * Defaults to the value provided by android.os.Build.VERSION.RELEASE
-	 */
-	public String getOsVersion() {
-		return osVersion;
-	}
+    /**
+     * The version of the operating system running.
+     * 
+     * Defaults to the value provided by android.os.Build.VERSION.RELEASE
+     */
+    public String getOsVersion() {
+        return osVersion;
+    }
 
-	/**
-	 * The version of the operating system running.
-	 * 
-	 * Defaults to the value provided by android.os.Build.VERSION.RELEASE
-	 */
-	public void setOsVersion(String osVersion) {
-		this.osVersion = osVersion;
-	}
+    /**
+     * The version of the operating system running.
+     * 
+     * Defaults to the value provided by android.os.Build.VERSION.RELEASE
+     */
+    public void setOsVersion(String osVersion) {
+        this.osVersion = osVersion;
+    }
 
-	/**
-	 * The Alias is an identifier of the user of the system.
-	 * 
-	 * Examples are an email address or a username
-	 * 
-	 */
-	public String getAlias() {
-		return alias;
-	}
+    /**
+     * The Alias is an identifier of the user of the system.
+     * 
+     * Examples are an email address or a username
+     * 
+     */
+    public String getAlias() {
+        return alias;
+    }
 
-	/**
-	 * The Alias is an identifier of the user of the system.
-	 * 
-	 * Examples are an email address or a username
-	 * 
-	 */
-	public void setAlias(String alias) {
-		this.alias = alias;
-	}
+    /**
+     * The Alias is an identifier of the user of the system.
+     * 
+     * Examples are an email address or a username
+     * 
+     */
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
 
-	/**
-	 * The category specifies a channel which may be used to send messages
-	 */
-	public String getCategory() {
-		return category;
-	}
+    /**
+     * The category specifies a channel which may be used to send messages
+     */
+    public String getCategory() {
+        return category;
+    }
 
-	/**
-	 * The category specifies a channel which may be used to send messages
-	 */
-	public void setCategory(String category) {
-		this.category = category;
-	}
+    /**
+     * The category specifies a channel which may be used to send messages
+     */
+    public void setCategory(String category) {
+        this.category = category;
+    }
 
 }
