cdc19924594d642d61e3a21ca50696b48ae59f46
Summers Pittman <secondsun@gmail.com>
2013-08-12 11:24:57 -0400

    Initial Push Support

---
+++ src/org/jboss/aerogear/android/unifiedpush/AGPushMessageReceiver.java
@@ -0,0 +1,70 @@
+/**
+ * JBoss, Home of Professional Open Source
+ * Copyright Red Hat, Inc., and individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.aerogear.android.unifiedpush;
+
+import android.app.Activity;
+import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.support.v4.app.NotificationCompat;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+import org.jboss.aerogear.R;
+
+public class AGPushMessageReceiver  extends BroadcastReceiver {
+
+    public static final int NOTIFICATION_ID = 1;
+
+    private NotificationManager mNotificationManager;
+    private Context ctx;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(context);
+        ctx = context;
+        String messageType = gcm.getMessageType(intent);
+        if (GoogleCloudMessaging.MESSAGE_TYPE_SEND_ERROR.equals(messageType)) {
+            sendNotification("Send error: " + intent.getExtras().toString());
+        } else if (GoogleCloudMessaging.MESSAGE_TYPE_DELETED.equals(messageType)) {
+            sendNotification("Deleted messages on server: " + intent.getExtras().toString());
+        } else {
+            sendNotification(intent.getStringExtra("alert"));
+        }
+        setResultCode(Activity.RESULT_OK);
+    }
+
+    // Put the GCM message into a notification and post it.
+    private void sendNotification(String msg) {
+        if(msg == null) { return; }
+
+        mNotificationManager = (NotificationManager) ctx.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(ctx)
+                // TODO Configurable title
+                .setContentTitle("GCM Notification")
+                // TODO Configurable icon
+                .setSmallIcon(R.drawable.common_signin_btn_icon_dark)
+                // TODO Change to received sound
+                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                .setStyle(new NotificationCompat.BigTextStyle().bigText(msg))
+                .setContentText(msg);
+
+        mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
+    }
+
+}
